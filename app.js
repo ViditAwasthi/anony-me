f2062e41e4c976abd28feceb01b02316c45e14a5
Vidit Awasthi <viditawasthi2@gmail.com>
2021-03-29 13:19:52 +0530

    added env Variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,8 +16,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-const secret = "Thisismytopsecret.";
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt,{ secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
